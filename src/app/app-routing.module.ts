66c13911ae1c70c981611346f2dba442084d2dad
sebbe1236 <74545889+sebbe1236@users.noreply.github.com>
2023-01-27 15:18:52 +0100

    view cart Items page. Clear each item button next.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './cart/cart.component';
 import { HomeComponent } from './home/home.component';
 import { ProductsComponent } from './products/products.component';
 import { SingleproductComponent } from './singleproduct/singleproduct.component';
@@ -8,6 +9,7 @@ const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'products', component: ProductsComponent },
   { path: 'product/:id', component: SingleproductComponent },
+  { path: 'cart', component: CartComponent },
 ];
 
 @NgModule({
--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { CartService } from '../services/cart.service';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['./cart.component.css'],
 })
 export class CartComponent {
+  products = this.cartService.getItems();
 
+  constructor(private cartService: CartService) {}
 }
