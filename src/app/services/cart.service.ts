ec2cad823f9038ce7c3a0f8aebe16eaa9b865f29
sebbe1236 <74545889+sebbe1236@users.noreply.github.com>
2023-02-01 15:34:34 +0100

    Single item removed from localstorage

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -6,7 +6,7 @@ import Product from '../products';
   providedIn: 'root',
 })
 export class CartService {
-  //Next up fix button to remove each item from cart.
+  //Next up fix that it`s possible to add more then 1 of each in storage and single item cleared from storage and not all.
   //source: https://angular.io/start/start-data
   cartProducts: Product[] = [];
 
@@ -33,7 +33,7 @@ export class CartService {
   // }
   clearCart(id: any) {
     this.cartProducts = this.cartProducts.filter((item) => item.id !== id);
-    localStorage.removeItem('cart_items');
+    localStorage.setItem('cart_items', JSON.stringify(this.cartProducts));
     return this.cartProducts;
   }
 }
